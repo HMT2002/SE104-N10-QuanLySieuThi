60301249002816cb0fe9b146bb1e3b8f051278b6
HMT <82308907+HMT2002@users.noreply.github.com>
2022-04-07 09:39:43 +0700

    thêm class SanPham

--- classes/SanPham.cs
+++ classes/SanPham.cs
@@ -8,5 +8,28 @@ namespace SE104_N10_QuanLySieuThi.classes
 {
     class SanPham
     {
+        private int id;
+
+        private HinhAnh anh;
+
+        private string name;
+
+        private string dvt;
+
+        private int amount;
+
+        private float price;
+
+        private string xuatxu;
+
+
+
+        public HinhAnh Anh { get => anh; set => anh = value; }
+        public string Name { get => name; set => name = value; }
+        public int Amount { get => amount; set => amount = value; }
+        public float Price { get => price; set => price = value; }
+        public int Id { get => id; set => id = value; }
+        public string Dvt { get => dvt; set => dvt = value; }
+        public string Xuatxu { get => xuatxu; set => xuatxu = value; }
     }
 }
