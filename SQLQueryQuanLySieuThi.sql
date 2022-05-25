85c279a8cf2a45675bedac75a47f1319378250a9
HMT <82308907+HMT2002@users.noreply.github.com>
2022-05-26 00:59:39 +0700

    scripts

--- SQLQueryQuanLySieuThi.sql
+++ SQLQueryQuanLySieuThi.sql
@@ -169,6 +169,6 @@ insert into ACCOUNT(ACC,PRI,PASS) values('huy',2,'1')
 
 
 select * from ACCOUNT 
-delete from ACCOUNT where ACC='tuee'
+delete from ACCOUNT where ACC!='tue'
 alter table ACCOUNT alter column PASS nvarchar(MAX)
 select * from KHACHHANG
