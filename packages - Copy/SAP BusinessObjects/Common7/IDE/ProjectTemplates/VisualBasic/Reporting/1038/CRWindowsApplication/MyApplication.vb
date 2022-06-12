69e54b937fbd8c61022cf9afed19069fbcfc54c5
HMT <82308907+HMT2002@users.noreply.github.com>
2022-06-12 16:35:24 +0700

    copy

---
+++ packages - Copy/SAP BusinessObjects/Common7/IDE/ProjectTemplates/VisualBasic/Reporting/1038/CRWindowsApplication/MyApplication.vb
@@ -0,0 +1,21 @@
+﻿Option Strict On
+Option Explicit On
+
+
+Namespace My
+    
+    Partial Class MyApplication
+        
+        <Global.System.Diagnostics.DebuggerStepThrough()> Public Sub New()
+            MyBase.New(ApplicationServices.AuthenticationMode.Windows)
+            Me.IsSingleInstance = False
+            Me.EnableVisualStyles = True
+            Me.SaveMySettingsOnExit = True
+            Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
+        End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThrough()> Protected Overrides Sub OnCreateMainForm()
+            Me.MainForm = Global.$safeprojectname$.Form1
+        End Sub
+    End Class
+End Namespace
